226c2d4d264ddfe0542c35b95bc4b48d3a9a8811
Emre Can Kurt <can.kurt764@gmail.com>
2024-11-29 23:03:55 +0300

    deneme

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Camera } from 'lucide-react';
 import './App.css';
 
 function App() {
-  return <Camera size={48} fill="red" />;
+  return <Camera size={48} fill="blue" />;
 }
 
 export default App;
