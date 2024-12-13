0fce8c4ea2fdc5ef69b536bc21a65255ef212990
Emre Can Kurt <can.kurt764@gmail.com>
2024-12-13 16:38:11 +0300

    Tailwinds check has been made and add applys

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,3 +1,15 @@
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      <h1>deneme 123</h1>
+      <h2>deneme 123</h2>
+      <h3>deneme 123</h3>
+      <h4>deneme 123</h4>
+      <h5>deneme 123</h5>
+      <h6>deneme 123</h6>
+      <span>btn-text</span>
+      <span>mobile-menu</span>
+      <span>paragraph</span>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,208 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: ['class'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  layers: ['base', 'components', 'utilities'],
   theme: {
-    extend: {},
+    extend: {
+      colors: { primary: '#23856D', secondary: '#47AD97', accent: '#17213C' },
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      fontSize: {
+        sm: [
+          '0.875rem',
+          {
+            lineHeight: '1.25rem',
+          },
+        ],
+        base: [
+          '1rem',
+          {
+            lineHeight: '1.375rem',
+          },
+        ],
+        '2xl': [
+          '1.5rem',
+          {
+            lineHeight: '2.8125rem',
+          },
+        ],
+        '3xl': [
+          '1.875rem',
+          {
+            lineHeight: '3.625rem',
+          },
+        ],
+        '4xl': [
+          '2.25rem',
+          {
+            lineHeight: '4.938rem',
+          },
+        ],
+        h1: {
+          sm: [
+            '1.5rem',
+            {
+              lineHeight: '2.8125rem',
+              letterSpacing: '0.0125rem',
+              fontWeight: '700',
+            },
+          ],
+          md: [
+            '1.875rem',
+            {
+              lineHeight: '3.625rem',
+              letterSpacing: '0.0125rem',
+              fontWeight: '700',
+            },
+          ],
+          lg: [
+            '2.25rem',
+            {
+              lineHeight: '4.938rem',
+              letterSpacing: '0.0125rem',
+              fontWeight: '700',
+            },
+          ],
+        },
+        h2: {
+          sm: [
+            '1.25rem',
+            {
+              lineHeight: '2.5rem',
+              letterSpacing: '0.0125rem',
+              fontWeight: '700',
+            },
+          ],
+          md: [
+            '1.5rem',
+            {
+              lineHeight: '3rem',
+              letterSpacing: '0.0125rem',
+              fontWeight: '700',
+            },
+          ],
+          lg: [
+            '1.875rem',
+            {
+              lineHeight: '3.625rem',
+              letterSpacing: '0.0125rem',
+              fontWeight: '700',
+            },
+          ],
+        },
+        h3: {
+          sm: [
+            '0.875rem',
+            {
+              lineHeight: '1.5rem',
+              letterSpacing: '0.00625rem',
+              fontWeight: '400',
+            },
+          ],
+          md: [
+            '0.9375rem',
+            {
+              lineHeight: '1.75rem',
+              letterSpacing: '0.00625rem',
+              fontWeight: '400',
+            },
+          ],
+          lg: [
+            '1rem',
+            {
+              lineHeight: '1.875rem',
+              letterSpacing: '0.00625rem',
+              fontWeight: '400',
+            },
+          ],
+        },
+        h4: {
+          sm: [
+            '0.875rem',
+            {
+              lineHeight: '1.5rem',
+              letterSpacing: '0.0125rem',
+              fontWeight: '700',
+            },
+          ],
+          md: [
+            '0.9375rem',
+            {
+              lineHeight: '1.75rem',
+              letterSpacing: '0.0125rem',
+              fontWeight: '700',
+            },
+          ],
+          lg: [
+            '1rem',
+            {
+              lineHeight: '1.875rem',
+              letterSpacing: '0.0125rem',
+              fontWeight: '700',
+            },
+          ],
+        },
+        h5: {
+          sm: [
+            '0.875rem',
+            {
+              lineHeight: '1.375rem',
+              letterSpacing: '0.00625rem',
+              fontWeight: '700',
+            },
+          ],
+          md: [
+            '0.9375rem',
+            {
+              lineHeight: '1.5rem',
+              letterSpacing: '0.00625rem',
+              fontWeight: '700',
+            },
+          ],
+          lg: [
+            '1rem',
+            {
+              lineHeight: '1.625rem',
+              letterSpacing: '0.00625rem',
+              fontWeight: '700',
+            },
+          ],
+        },
+        h6: {
+          sm: [
+            '0.75rem',
+            {
+              lineHeight: '1rem',
+              letterSpacing: '0.0125rem',
+              fontWeight: '600',
+            },
+          ],
+          md: [
+            '0.8125rem',
+            {
+              lineHeight: '1.125rem',
+              letterSpacing: '0.0125rem',
+              fontWeight: '600',
+            },
+          ],
+          lg: [
+            '0.875rem',
+            {
+              lineHeight: '1.25rem',
+              letterSpacing: '0.0125rem',
+              fontWeight: '600',
+            },
+          ],
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')],
 };
