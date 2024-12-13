2270fe359c59db421aafddfa2b1e9a4fbc368ab4
Emre Can Kurt <can.kurt764@gmail.com>
2024-12-13 16:35:16 +0300

    routes were designed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
-import { Camera } from 'lucide-react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import PageContent from './layouts/PageContent';
+import HomePage from './pages/HomePage';
+import Header from './layouts/Header';
+import Footer from './layouts/Footer';
 
 function App() {
-  return <Camera size={48} fill="blue" />;
+  return (
+    <>
+      <Header />
+      <PageContent>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+      </PageContent>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
+
+/*
+header
+pc
+switch
+.
+.
+.
+switch
+pc
+footer
+*/
